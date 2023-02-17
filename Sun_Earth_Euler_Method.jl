6cca24067fb513134046cda087c2b78c0002ea5b
hosseinsharif9 <hosseinsharif9@gmail.com>
2023-02-17 13:53:25 +0330

    Use plotlyjs()

--- Sun_Earth_Euler_Method.jl
+++ Sun_Earth_Euler_Method.jl
@@ -2,18 +2,17 @@
 
 #= Sun-Earth Problem:
     Solving The Differential Equation of Earth Displacement And Velocity Vector 
+    With Simple Euler Method
 =#
 
 #= Note!
     In genral we can say that any other system of two masses with mass of the one
     of them very greater than the other, will go this way! =#
 
-closeall
-
 using Plots
 
 dt = 0.01
-t = 0:dt:748.5
+t = 0:dt:50
 G = 0.01
 M_S = 0.1
 
@@ -24,26 +23,26 @@ xdot0 = -0.03
 ydot0 = 0.01
 zdot0 = 0
 
-alpha = zeros(1,1)
-beta = zeros(1,1)
+alpha = zeros(1, 1)
+beta = zeros(1, 1)
 
-x = zeros(6,length(t))
-x[:,1] = [x0 y0 z0 xdot0 ydot0 zdot0]'
+x = zeros(6, length(t))
+x[:, 1] = [x0 y0 z0 xdot0 ydot0 zdot0]'
 
 for i = 1:(length(t)-1)
-    alpha = 1/((x[1,i]^2 + x[2,i]^2 + x[3,i]^2)^(3/2))
-    beta = -G*M_S*alpha
+    alpha = 1 / ((x[1, i]^2 + x[2, i]^2 + x[3, i]^2)^(3 / 2))
+    beta = -G * M_S * alpha
 
-    A = [0         0         0         1         0         0;
-         0         0         0         0         1         0;
-         0         0         0         0         0         1;
-         beta      0         0         0         0         0;
-         0         beta      0         0         0         0;
-         0         0         beta      0         0         0]
+    A = [0 0 0 1 0 0
+        0 0 0 0 1 0
+        0 0 0 0 0 1
+        beta 0 0 0 0 0
+        0 beta 0 0 0 0
+        0 0 beta 0 0 0]
 
-    x[:,i+1] = x[:,i] + (dt.*A)*x[:,i]
+    x[:, i+1] = x[:, i] + (dt .* A) * x[:, i]
 end
 
-gr()
+plotlyjs()
 
-plot(x[1,:],x[2,:])
+plot(x[1, :], x[2, :])
